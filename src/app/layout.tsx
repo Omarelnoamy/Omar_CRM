3c889241d74f3c5b8b60b2f2f5a4cfda3fafc4b1
epnu <epnu@epnus-MacBook-Pro.local>
2026-03-17 11:30:55 +0200

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import { Roboto, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -19,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={`${roboto.className} antialiased`}>
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
           {children}
